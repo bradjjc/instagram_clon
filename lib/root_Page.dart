fdadd3ce3fb1f0bc76a7a0310727999889f599fb
bradjjc <jichangjung@yahoo.com>
2021-04-12 16:02:35 +0900

    lognin

--- lib/root_Page.dart
+++ lib/root_Page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clon/tap_Page.dart';
 
+import 'login_Page.dart';
+
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabPage();
+    return LoginPage();
 
   }
 }
