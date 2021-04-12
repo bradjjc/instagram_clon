0c1793c863f788c12f1464e632894565073a4461
bradjjc <jichangjung@yahoo.com>
2021-04-12 12:09:31 +0900

    뼈대

--- lib/root_Page.dart
+++ lib/root_Page.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clon/tap_Page.dart';
 
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabPage();
+
+  }
+}
---
+++ lib/tap_Page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
+  List _paged = [
+    Text('page1'),
+    Text('page2'),
+    Text('page3'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _paged[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+          currentIndex: _selectedIndex,
+          items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Account'),
+      ]),
+    );
+  }
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
